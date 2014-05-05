a99e740094b5a2f59142bd271430c8eda932457e
felipedaraujo <daraujofelipe@gmail.com>
2014-05-05 10:04:36 -0500

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the content 'Alpha Bookmarking'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Alpha Bookmarking')
     end
   end
+
+  describe "About page" do
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+  end
+
 end
